b41223d689543227ac3b432d2cc4731e6a8db803
alextsapin <alextsapin@yandex.ru>
2022-10-17 20:56:50 +0200

    Add login form

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,11 +1,105 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import {useFormik} from 'formik';
+import TextField from '@mui/material/TextField';
+import {Button} from '@mui/material';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Alert from '@mui/material/Alert';
+import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
+import eye from '../../images/eye.png'
+import css from './css.module.scss';
+import { NavLink } from 'react-router-dom';
+
+type errorType = {
+    email: string
+    password: string
+}
 
 const Login = () => {
-    return (
-        <div>
-            LOGIN
-        </div>
-    );
-};
 
-export default Login;
+    const dispatch = useAppDispatch();
+
+    const [showPass, isShowPass] = React.useState(false);
+
+    function showPassHandler() {
+        isShowPass(!showPass)
+    }
+
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: Partial<errorType> = {};
+
+            if(!values.email) {
+                errors.email = 'Email is required';
+            }
+            else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Incorrect email address';
+            } 
+
+            if(!values.password) {
+                errors.password = 'Password is required';
+            }
+           
+            return errors
+        },
+        onSubmit: values => {
+            //dispatch(login(values))
+        },
+    });
+
+    return (
+        <Container fixed>
+            <form onSubmit={formik.handleSubmit} className={css.form}>
+                <p className={css.title}>Sign in</p>
+
+                <TextField
+                    type="text"
+                    label="Email"
+                    size="small"
+                    className={css.field}
+                    variant="standard"
+                    {...formik.getFieldProps('email')}
+                />
+
+                <div className={css.wrapper}>
+                    <TextField
+                        name="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        type={showPass === false ? 'password' : 'text'}
+                        label="Password"
+                        size="small"
+                        variant="standard"
+                        className={css.password}
+                    />
+                    <button className={css.eye} onClick={showPassHandler}>
+                        <img src={eye} alt="eye"/>
+                    </button>
+                </div>
+
+                <FormControlLabel 
+                    className={css.checkbox}
+                    control={<Checkbox name="rememberMe" value={formik.values.rememberMe} onChange={formik.handleChange}/>} 
+                    label="Remember me"
+                />
+
+<NavLink className={css.register} to="/registration">Sign Up</NavLink>
+
+                <button type="submit" className={css.button}>Sign in</button>
+
+                <p className={css.attention}>Already have an account?</p>
+
+                <NavLink className={css.register} to="/registration">Sign Up</NavLink>
+            </form>
+        </Container>
+    )
+}
+
+export default Login
---
+++ src/redux/store.ts
@@ -0,0 +1,21 @@
+import {legacy_createStore as createStore, combineReducers, applyMiddleware, AnyAction} from 'redux';
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk';
+
+
+// Объединим редьюсеры
+const rootReducer = combineReducers({
+
+});
+
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+type rootReducerType = typeof rootReducer;
+
+export type AppStateType = ReturnType<rootReducerType>;
+export type AppDispatch = ThunkDispatch<AppStateType, unknown, AnyAction>
+
+// 1 — Что возвращает thunk
+// 2 — Тип стейта всего приложения
+// 3 — Экстра–аргументы
+// 4 — Типы всех actions
+export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, AnyAction>
