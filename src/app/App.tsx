b5f727bbc393f521bb30e04f6fa65d81e5edeb93
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-12-25 03:25:10 +0300

    fix: rewriting css to MUI styled components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -25,7 +25,7 @@ export const App = () => {
             {
                 isLoaded && <ThemeProvider theme={customTheme}>
                     <HashRouter>
-                        {/*<PageNavigation/>*/}
+                        <PageNavigation/>
                         <Pages/>
                     </HashRouter>
                 </ThemeProvider>
