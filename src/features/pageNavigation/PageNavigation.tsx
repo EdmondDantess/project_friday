38ef515e4f4045d01f61c28524492c77b994d011
dmitrykrtv <dmitry.korotayev@gmail.com>
2023-01-04 21:27:21 +0300

    fix: migrate AppNavBar from scss to mui components

--- src/features/pageNavigation/PageNavigation.tsx
+++ src/features/pageNavigation/PageNavigation.tsx
@@ -5,24 +5,23 @@ import {
     Avatar,
     Box,
     Button,
-    CircularProgress,
     Container,
     IconButton,
     Menu,
     MenuItem,
+    styled,
     Toolbar,
     Tooltip,
     Typography
 } from "@mui/material";
-import styles from "./pageNavigation.module.scss";
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
-import {ErrorSnackbar} from "../../common/components/errorSnackbar/ErrorSnackbar";
 import {logoutTC} from "../profile/profile-reducer";
 import {PATH} from "../pages/Pages";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import InventoryIcon from "@mui/icons-material/Inventory";
 import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 import {ThemeSwitch} from "../../common/components/themeSwitch/ThemeSwitch";
+import Logo from "../../assets/logo.svg"
 
 const userMenuElements: { title: string, componentLink: string, icon: JSX.Element }[] = [
     {
@@ -84,72 +83,72 @@ export const PageNavigation = () => {
 
     return (
         <>
-            <div className={styles.AppNavBar}>
-                <div className={styles.logoContainer} onClick={() => navigate(PATH.PROFILE)}>
-                    <div className={styles.itIncubLogo}></div>
-                </div>
-                <div className={styles.buttonLogContainer}>
-                    {
-                        isLogged
-                            ?
-                            <div className={styles.divProfileHeader}
-                            >
-                                <ThemeSwitch/>
-                                <b style={{margin: "5px 10px 0 0 "}} onClick={handleOpenUserMenu}>{name}
-                                    <hr/>
-                                </b>
-                                <Box sx={{flexGrow: 0}}>
-                                    <Tooltip title="Open settings">
-                                        <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
-                                            <Avatar alt={name !== "" ? name : "fail"}
-                                                    src={avatar ? avatar : "https://bit.ly/3CKLqoF"}
-                                                    sx={{width: 36, height: 36}}/>
-                                        </IconButton>
-                                    </Tooltip>
-                                    <Menu
-                                        sx={{mt: "45px"}}
-                                        id="menu-appbar"
-                                        anchorEl={anchorElUser}
-                                        anchorOrigin={{
-                                            vertical: "top",
-                                            horizontal: "right",
-                                        }}
-                                        keepMounted
-                                        transformOrigin={{
-                                            vertical: "top",
-                                            horizontal: "right",
-                                        }}
-                                        open={Boolean(anchorElUser)}
-                                        onClose={handleCloseMenu}
-                                    >
-                                        {userMenuElements.map((navLink, index) => (
-                                            <MenuItem key={index} onClick={handleCloseUserMenu(navLink.componentLink)}>
-                                                {navLink.icon}
-                                                <Typography textAlign="center"
-                                                            sx={{margin: "0 0 0 5px"}}>{navLink.title}</Typography>
-                                            </MenuItem>
-                                        ))}
-                                    </Menu>
-                                </Box>
-                            </div>
+            {/*<div className={styles.AppNavBar}>*/}
+            {/*    <div className={styles.logoContainer} onClick={() => navigate(PATH.PROFILE)}>*/}
+            {/*        <div className={styles.itIncubLogo}></div>*/}
+            {/*    </div>*/}
+            {/*    <div className={styles.buttonLogContainer}>*/}
+            {/*        {*/}
+            {/*            isLogged*/}
+            {/*                ?*/}
+            {/*                <div className={styles.divProfileHeader}*/}
+            {/*                >*/}
+            {/*                    <ThemeSwitch/>*/}
+            {/*                    <b style={{margin: "5px 10px 0 0 "}} onClick={handleOpenUserMenu}>{name}*/}
+            {/*                        <hr/>*/}
+            {/*                    </b>*/}
+            {/*                    <Box sx={{flexGrow: 0}}>*/}
+            {/*                        <Tooltip title="Open settings">*/}
+            {/*                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>*/}
+            {/*                                <Avatar alt={name !== "" ? name : "fail"}*/}
+            {/*                                        src={avatar ? avatar : "https://bit.ly/3CKLqoF"}*/}
+            {/*                                        sx={{width: 36, height: 36}}/>*/}
+            {/*                            </IconButton>*/}
+            {/*                        </Tooltip>*/}
+            {/*                        <Menu*/}
+            {/*                            sx={{mt: "45px"}}*/}
+            {/*                            id="menu-appbar"*/}
+            {/*                            anchorEl={anchorElUser}*/}
+            {/*                            anchorOrigin={{*/}
+            {/*                                vertical: "top",*/}
+            {/*                                horizontal: "right",*/}
+            {/*                            }}*/}
+            {/*                            keepMounted*/}
+            {/*                            transformOrigin={{*/}
+            {/*                                vertical: "top",*/}
+            {/*                                horizontal: "right",*/}
+            {/*                            }}*/}
+            {/*                            open={Boolean(anchorElUser)}*/}
+            {/*                            onClose={handleCloseMenu}*/}
+            {/*                        >*/}
+            {/*                            {userMenuElements.map((navLink, index) => (*/}
+            {/*                                <MenuItem key={index} onClick={handleCloseUserMenu(navLink.componentLink)}>*/}
+            {/*                                    {navLink.icon}*/}
+            {/*                                    <Typography textAlign="center"*/}
+            {/*                                                sx={{margin: "0 0 0 5px"}}>{navLink.title}</Typography>*/}
+            {/*                                </MenuItem>*/}
+            {/*                            ))}*/}
+            {/*                        </Menu>*/}
+            {/*                    </Box>*/}
+            {/*                </div>*/}
 
-                            : <>
-                                <ThemeSwitch/>
-                                <Button type={"submit"}
-                                        variant={"contained"}
-                                        color={"primary"}
-                                        className={styles.buttonLog}
-                                        onClick={redirectsLoginHandler}
-                                >{routesButtonHead}</Button>
-                            </>
-                    }
-                </div>
+            {/*                : <>*/}
+            {/*                    <ThemeSwitch/>*/}
+            {/*                    <Button type={"submit"}*/}
+            {/*                            variant={"contained"}*/}
+            {/*                            color={"primary"}*/}
+            {/*                            className={styles.buttonLog}*/}
+            {/*                            onClick={redirectsLoginHandler}*/}
+            {/*                    >{routesButtonHead}</Button>*/}
+            {/*                </>*/}
+            {/*        }*/}
+            {/*    </div>*/}
 
-                <ErrorSnackbar/>
+            {/*    <ErrorSnackbar/>*/}
 
-                {circularEntity && <CircularProgress className={styles.progress}/>}
+            {/*    {circularEntity && <CircularProgress className={styles.progress}/>}*/}
 
-            </div>
+            {/*</div>*/}
             <ResponsiveAppBar/>
         </>
     );
@@ -158,75 +157,168 @@ export const PageNavigation = () => {
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
+
+    const circularEntity = useAppSelector(state => state.userFeedback.circularEntity)
+    const isLogged = useAppSelector(state => state.profile.isLogged)
+    const avatar = useAppSelector(state => state.profile.avatar)
+    const name = useAppSelector(state => state.profile.name)
+
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    let routesButtonHead = useRoutes([
+        {path: PATH.LOGIN, element: <span>Sign up</span>},
+        {path: "*", element: <span>Sign in</span>},
+    ])
+
+    const redirectsLoginHandler = () => {
+        location.pathname === `${PATH.LOGIN}` ? navigate(PATH.REGISTRATION) : navigate(PATH.LOGIN)
+    }
+
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElUser(event.currentTarget);
     };
 
-    const handleCloseUserMenu = () => {
+    const handleCloseUserMenu = (link: string) => {
+        return () => {
+            if (link === PATH.LOGIN) {
+                dispatch(logoutTC())
+            }
+            setAnchorElUser(null);
+            navigate(link)
+        }
+    };
+
+    const handleCloseMenu = () => {
         setAnchorElUser(null);
     };
 
     return (
-        <AppBar position="static" sx={{backgroundColor: "var(--bg1)", backgroundImage: "none"}}>
-            <Container maxWidth="xl" sx={{backgroundColor: "var(--bg1)"}}>
+        <AppBar position="static" sx={{backgroundColor: "var(--bg1)", backgroundImage: "none", height: "60px"}}>
+            <NavbarContainer sx={{height: "60px"}}>
                 <Toolbar disableGutters>
+                    <Box
+                        component={"img"}
+                        sx={{
+                            width: "50px",
+                            height: "50px",
+                            color: "var(--text-color1)",
+                            mb: "5px",
+                        }}
+                        alt={"Quiz Logo"}
+                        src={Logo}
+                    />
                     <Typography
                         variant="h6"
-                        noWrap
+                        // noWrap
                         component="a"
                         href="/"
                         sx={{
                             width: "90px",
                             mr: 2,
-                            display: { xs: 'none', md: 'flex' },
+                            display: {xs: 'none', sm: 'flex'},
                             fontFamily: 'monospace',
                             fontWeight: 700,
                             letterSpacing: '.3rem',
-                            color: 'inherit',
+                            color: "var(--text-color1)",
                             textDecoration: 'none',
                         }}
                     >
-                        LOGO
+                        QUIZ
                     </Typography>
 
-                    <Box sx={{flex: "1 1 100%"}} />
+                    <Box sx={{flex: "1 1 100%"}}/>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center">{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
+                    <Box sx={{flexGrow: 0}}>
+                        {
+                            isLogged
+                                ?
+                                <Box sx={{display: "flex"}}>
+                                    <ThemeSwitch/>
+                                    <Typography
+                                        onClick={handleOpenUserMenu}
+                                        noWrap
+                                        sx={{
+                                            mr: "10px",
+                                            width: "120px",
+                                            fontWeight: 700,
+                                            textDecoration: "underline",
+
+                                            cursor: "pointer",
+                                            color: "var(--text-color1)",
+
+                                            display: "flex",
+                                            alignItems: "center",
+                                        }}
+                                    >
+                                        {name.length > 11 ? `${name.slice(0, 11)}...` : name}
+                                    </Typography>
+                                    <Box sx={{flexGrow: 0}}>
+                                        <Tooltip title="Open settings">
+                                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                                <Avatar alt={name !== "" ? name : "fail"}
+                                                        src={avatar ? avatar : "https://bit.ly/3CKLqoF"}
+                                                        sx={{width: 36, height: 36}}/>
+                                            </IconButton>
+                                        </Tooltip>
+                                        <Menu
+                                            sx={{mt: "45px"}}
+                                            id="menu-appbar"
+                                            anchorEl={anchorElUser}
+                                            anchorOrigin={{
+                                                vertical: "top",
+                                                horizontal: "right",
+                                            }}
+                                            keepMounted
+                                            transformOrigin={{
+                                                vertical: "top",
+                                                horizontal: "right",
+                                            }}
+                                            open={Boolean(anchorElUser)}
+                                            onClose={handleCloseMenu}
+                                        >
+                                            {userMenuElements.map((navLink, index) => (
+                                                <MenuItem key={index}
+                                                          onClick={handleCloseUserMenu(navLink.componentLink)}>
+                                                    {navLink.icon}
+                                                    <Typography textAlign="center"
+                                                                sx={{margin: "0 0 0 5px"}}>{navLink.title}</Typography>
+                                                </MenuItem>
+                                            ))}
+                                        </Menu>
+                                    </Box>
+                                </Box>
+                                : <Box sx={{display: "flex"}}>
+                                    <ThemeSwitch/>
+                                    <Button type={"submit"}
+                                            variant={"contained"}
+                                            color={"primary"}
+                                            onClick={redirectsLoginHandler}
+                                            sx={{height: "36px", width: "114px"}}
+                                    >{routesButtonHead}</Button>
+                                </Box>
+                        }
                     </Box>
                 </Toolbar>
-            </Container>
+            </NavbarContainer>
         </AppBar>
     );
 }
+
+export const NavbarContainer = styled(Container)(({theme}) => ({
+    backgroundColor: "var(--bg1)",
+
+    maxWidth: theme.breakpoints.values.lg,
+    [theme.breakpoints.down("lg")]: {
+        maxWidth: theme.breakpoints.values.md,
+    },
+    [theme.breakpoints.down("md")]: {
+        maxWidth: "100%",
+    },
+}));
 
 export default ResponsiveAppBar;
 
